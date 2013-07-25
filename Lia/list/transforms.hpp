8f1c1793d854f498513eebbb9fdf3077b0f13a4e
Rapptz <rapptz@gmail.com>
2013-07-25 08:18:14 -0400

    Added transforms.hpp

---
+++ Lia/list/transforms.hpp
@@ -0,0 +1,67 @@
+#ifndef LIA_TRANSFORMS_LIST_COMP_HPP
+#define LIA_TRANSFORMS_LIST_COMP_HPP
+
+// Includes
+#include "../detail/iterator.hpp"
+#include "../detail/type_traits.hpp"
+#include <algorithm>
+
+namespace lia {
+template<class Cont, typename Map>
+inline auto map(const Cont& cont, Map&& mapped) -> Rebind<Cont, ResultOf<Unqualified<Map>>> {
+    Rebind<Cont, ResultOf<Unqualified<Map>>> result;
+    std::transform(std::begin(cont),std::end(cont), std::back_inserter(result),mapped);
+    return result;
+}
+
+template<class Cont>
+inline Unqualified<Cont> reverse(Cont&& cont) {
+    Unqualified<Cont> result;
+    std::reverse_copy(std::begin(cont),std::end(cont),std::back_inserter(result));
+    return result;
+}
+
+template<class Cont>
+inline Unqualified<Cont> intersperse(const Cont& cont, ValueType<Unqualified<Cont>> elem) {
+    Unqualified<Cont> result;
+    auto first = std::begin(cont);
+    auto last = std::end(cont);
+    if(first != last)
+        result.push_back(*first++);
+    while(first != last) {
+        result.push_back(elem);
+        result.push_back(*first++);
+    }
+    return result;
+}
+
+template<class Cont>
+inline auto subsequences(const Cont& cont) -> Rebind<Cont, Unqualified<Cont>> {
+    Rebind<Cont, Unqualified<Cont>> result;
+    auto first = std::begin(cont);
+    Unqualified<Cont> temp;
+    auto max = 1ULL << cont.size();
+    for(unsigned i = 0; i < max; ++i) {
+        for(unsigned j = 0, h = i; h; h >>= 1, ++j) {
+            if(h & 1)
+                temp.push_back(*(first + j));
+        }
+        result.push_back(temp);
+        temp.clear();
+    }
+    return result;
+}
+
+template<class Cont>
+inline auto permutations(Cont&& cont) -> Rebind<Unqualified<Cont>, Unqualified<Cont>> {
+    Rebind<Unqualified<Cont>, Unqualified<Cont>> result;
+    Unqualified<Cont> temp(std::forward<Cont>(cont));
+    do {
+        result.push_back(temp);
+    }
+    while(std::next_permutation(std::begin(temp), std::end(temp)));
+    return result;
+}
+} // lia
+
+#endif //LIA_TRANSFORMS_LIST_COMP_HPP
--- test.cpp
+++ test.cpp
@@ -1,6 +1,26 @@
 #include "Lia/list/basic.hpp"
+#include "Lia/list/transforms.hpp"
 #include <vector>
 #include <iostream>
+
+template<class Elem, class Traits, class Cont>
+std::basic_ostream<Elem,Traits>& operator<<(std::basic_ostream<Elem,Traits>& out, const Cont& cont) {
+    auto first = std::begin(cont);
+    auto last = std::end(cont);
+    if(first == last) {
+        out << "[]";
+        return out;
+    }
+    else {
+        out << '[' << *first++;
+    }
+    while(first != last) {
+        out << ',' << *first++;
+    }
+    out << ']';
+    return out;
+}
+
 
 void basic_test() {
     std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11};
@@ -9,17 +29,21 @@ void basic_test() {
     std::cout << " Length: " << lia::length(v);
     auto g = lia::tail(v);
     auto appended = lia::append(v, g);
-    std::cout << "\nTail: ";
-    for(auto&& i : g)
-        std::cout << i << ' ';
-    std::cout << "\nRegular: ";
-    for(auto&& i : v)
-        std::cout << i << ' ';
-    std::cout << "\nAppended: ";
-    for(auto&& i : appended)
-        std::cout << i << ' ';
+    std::cout << "\nTail: " << g;
+    std::cout << "\nRegular: " << v;
+    std::cout << "\nAppended: " << appended << '\n';
+}
+
+void transform_test() {
+    std::vector<int> v = {1,2,3,4,5,6,7,8,9,10,11};
+    std::cout << "Map: " << lia::map(v, [](int v) { return v > 4; });
+    std::cout << "\nReverse: " << lia::reverse(v);
+    std::cout << "\nIntersperse: " << lia::intersperse(v, 0);
+    std::cout << "\nSubsequences: " << lia::subsequences(std::vector<int>{1,2,3,4});
+    std::cout << "\nPermutations: " << lia::permutations(std::vector<int>{1,2,3,4});
 }
 
 int main() {
     basic_test();
+    transform_test();
 }
