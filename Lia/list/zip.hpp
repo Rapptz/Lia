245bbfc3a5bf0957892dd87deae7ea502e3d00d0
Rapptz <rapptz@gmail.com>
2013-08-03 02:50:36 -0400

    Added zip (Issue #1)

---
+++ Lia/list/zip.hpp
@@ -0,0 +1,33 @@
+#ifndef LIA_ZIP_LIST_COMP_HPP
+#define LIA_ZIP_LIST_COMP_HPP
+
+#include "../detail/type_traits.hpp"
+#include <tuple>
+#include <vector>
+
+namespace lia {
+namespace detail {
+template<template<typename...> class Cont, typename T>
+const T& forward_index(const Cont<T>& cont, unsigned i) {
+    return cont[i];
+}
+ 
+template<template<typename...> class Cont, typename T>
+T&& forward_index(Cont<T>&& cont, unsigned i) {
+    return std::move(cont[i]);
+}
+} // detail
+
+template<typename... Args>
+std::vector<std::tuple<ValueType<Args>...>> zip(Args&&... args) {
+    auto size = min(args.size()...);
+    std::vector<std::tuple<ValueType<Args>...>> result;
+    result.reserve(size);
+    for(unsigned i = 0; i < size; ++i) {
+        result.emplace_back(detail::forward_index(std::forward<Args>(args), i)...);
+    }
+    return result;
+}
+} // lia
+
+#endif // LIA_ZIP_LIST_COMP_HPP
