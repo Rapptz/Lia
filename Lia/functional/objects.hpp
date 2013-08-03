4f976461ec3ff4b86c2d0cee8c260f78aa4810d7
Rapptz <rapptz@gmail.com>
2013-08-02 23:14:21 -0400

    Added universal object header

---
+++ Lia/functional/objects.hpp
@@ -0,0 +1,10 @@
+#ifndef LIA_FUNCTIONAL_OBJECTS_HPP
+#define LIA_FUNCTIONAL_OBJECTS_HPP
+
+#include "objects/arithmetic.hpp"
+#include "objects/bitwise.hpp"
+#include "objects/comparison.hpp"
+#include "objects/logical.hpp"
+#include "objects/numeric.hpp"
+
+#endif // LIA_FUNCTIONAL_OBJECTS_HPP
