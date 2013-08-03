0f0e42f14af5b1c40facb0775031b45c5620a4d9
Rapptz <rapptz@gmail.com>
2013-08-03 05:38:53 -0400

    Added zip_with (Issue #1)

--- Lia/list/zip.hpp
+++ Lia/list/zip.hpp
@@ -7,24 +7,35 @@
 
 namespace lia {
 namespace detail {
-template<template<typename...> class Cont, typename T>
-const T& forward_index(const Cont<T>& cont, unsigned i) {
+template<class Cont>
+inline const ValueType<Cont>& forward_index(const Cont& cont, unsigned i) {
     return cont[i];
 }
  
-template<template<typename...> class Cont, typename T>
-T&& forward_index(Cont<T>&& cont, unsigned i) {
+template<class Cont>
+inline ValueType<Cont>&& forward_index(Cont&& cont, unsigned i) {
     return std::move(cont[i]);
 }
 } // detail
 
 template<typename... Args>
-std::vector<std::tuple<ValueType<Args>...>> zip(Args&&... args) {
+inline std::vector<std::tuple<ValueType<Args>...>> zip(Args&&... args) {
     auto size = min(args.size()...);
     std::vector<std::tuple<ValueType<Args>...>> result;
     result.reserve(size);
     for(unsigned i = 0; i < size; ++i) {
         result.emplace_back(detail::forward_index(std::forward<Args>(args), i)...);
+    }
+    return result;
+}
+
+template<class Cont, class Function, typename... Args>
+inline auto zip_with(Function&& f, Cont cont, Args&&... args) -> Rebind<Cont, decltype(f(cont.back(), args.back()...))> {
+    Rebind<Cont, decltype(f(cont.back(), args.back()...))> result;
+    auto size = min(cont.size(), args.size()...);
+    for(unsigned i = 0; i < size; ++i) {
+        result.emplace_back(f(detail::forward_index(cont, i), 
+                              detail::forward_index(std::forward<Args>(args), i)...));
     }
     return result;
 }
