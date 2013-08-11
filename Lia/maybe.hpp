fbe20aab8956681fbc27578d4acd7f62a89c8dca
Rapptz <rapptz@gmail.com>
2013-08-11 04:08:27 -0400

    Moved just and nothing out of maybe folder

--- Lia/maybe.hpp
+++ Lia/maybe.hpp
@@ -1,6 +1,17 @@
-#ifndef LIA_MAYBE_SHORTCUT_HPP
-#define LIA_MAYBE_SHORTCUT_HPP
+#ifndef LIA_MAYBE_MASTER_HPP
+#define LIA_MAYBE_MASTER_HPP
 
 #include "maybe/maybe.hpp"
 
-#endif // LIA_MAYBE_SHORTCUT_HPP
+namespace lia {
+template<typename T>
+inline maybe<Decay<T>> just(T&& value) {
+    return maybe<Decay<T>>{std::forward<T>(value)};
+}
+
+inline maybe<nothing_t> nothing() {
+    return {};
+}
+} // lia
+
+#endif // LIA_MAYBE_MASTER_HPP
--- Lia/maybe/maybe.hpp
+++ Lia/maybe/maybe.hpp
@@ -87,15 +87,6 @@ public:
         return *value;
     }
 };
-
-template<typename T>
-inline maybe<Decay<T>> just(T&& value) {
-    return maybe<Decay<T>>{std::forward<T>(value)};
-}
-
-inline maybe<nothing_t> nothing() {
-    return {};
-}
 } // lia
 
 #endif // LIA_MAYBE_HPP
